c4c2b9faf9784ad6f5f93833daf7275607e743d4
DannyYanacallo1755 <danny9yanacallo@gmail.com>
2024-06-23 20:33:34 -0500

    Filtrado de datos

--- src/app/screens/listing/listing.page.ts
+++ src/app/screens/listing/listing.page.ts
@@ -12,6 +12,8 @@ import { FoodService } from 'src/app/services/food.service';
 export class ListingPage implements OnInit {
   categories: Category[] = [];
   foods: Food[] = [];
+  filteredFoods: Food[] = [];
+  selectedCategory: string = 'Todo';
 
   constructor(private foodService: FoodService, private router: Router) {}
 
@@ -19,6 +21,7 @@ export class ListingPage implements OnInit {
     this.getCategories();
     this.foodService.getFoods().subscribe((data) => {
       this.foods = data;
+      this.applyFilter();
     });
   }
 
@@ -32,23 +35,38 @@ export class ListingPage implements OnInit {
       },
       {
         id: 2,
-        label: 'Nuevo',
+        label: 'pajatoquilla',
         image: 'assets/images/icons/nuevo.png',
         active: false,
       },
       {
         id: 3,
-        label: 'Popular',
+        label: 'totora',
         image: 'assets/images/icons/popular.png',
         active: false,
       },
       {
         id: 4,
-        label: 'Ofertas',
+        label: 'Despues',
         image: 'assets/images/icons/oferta.png',
         active: false,
       },
     ];
+  }
+
+  applyFilter() {
+    if (this.selectedCategory === 'Todo') {
+      this.filteredFoods = this.foods;
+    } else {
+      this.filteredFoods = this.foods.filter(food => food.category === this.selectedCategory);
+    }
+  }
+
+  selectCategory(category: Category) {
+    this.selectedCategory = category.label;
+    this.categories.forEach(cat => cat.active = false);
+    category.active = true;
+    this.applyFilter();
   }
 
   goToDetailPage(id: string) {
