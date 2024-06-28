aac1742ccd35a795c0e444b7795bc7bd1c926f86
DannyYanacallo1755 <danny9yanacallo@gmail.com>
2024-06-28 17:24:31 -0500

    Pantalla de carga

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -11,22 +11,30 @@ export class LoginPage {
   email: string = '';
   password: string = '';
   error: string = '';
+  loading: boolean = false;
 
   constructor(private authService: AuthService, private router: Router) {}
 
   login() {
+    this.loading = true;
+    this.error = '';
+    
     this.authService.login(this.email, this.password).subscribe(
       (response) => {
         if (response && response.token) {
           // Credenciales válidas, redirigir al home
-          this.router.navigate(['/home']);
+          this.router.navigate(['/home']).then(() => {
+            this.loading = false;
+          });
         } else {
           // Credenciales inválidas, mostrar mensaje de error
+          this.loading = false;
           this.error = 'Credenciales inválidas';
         }
       },
       (error) => {
-        this.error = error;
+        this.loading = false;
+        this.error = 'Error en el inicio de sesión. Por favor, inténtelo de nuevo.';
       }
     );
   }
